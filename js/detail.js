310f57ce8a3c2ec6eea28095965d1eeb98d92053
different650 <ehdrmftm@naver.com>
2025-12-08 14:20:24 +0900

    system file

---
+++ js/detail.js
@@ -0,0 +1,303 @@
+$(function () {
+    floatingControl();// panel & popup
+    toggleControl();// toggle(search)
+    mainSlider();
+    mobileMenu();
+    mobileFooter("footer > div:nth-of-type(2) > div:not(:first-of-type) > h3");
+    eyeIcon();
+    nestedPopup();
+    filterControl();
+    mobileFilter();
+    listaccControl(".listContainer .filterPanel fieldset legend" );
+    listHover();
+    PromotionSlider();
+    joySlider();
+    backControl();
+    tabMenu();
+    accControl(".accComponent li h3");
+
+    imgSwap();
+    // colorTabUI();
+    // productControl();
+    countControl();
+});
+
+// panel & popup
+function floatingControl() {
+    var currentTarget = null;
+    $("[type='button']").click(function () {
+        if ($(this).attr("data-panel")) {
+            currentTarget = "." + $(this).attr("data-panel");
+            $(currentTarget).addClass("active");
+        } else if ($(this).attr("data-popup")) {
+            currentTarget = "." + $(this).attr("data-popup");
+            $(currentTarget).addClass("active");
+        }
+    });
+    // 여는파트
+    $('[class$="CloseBtn"]').click(function () {
+        // 위 대상은 $=CloseBtn으로 정리
+        $(currentTarget).removeClass("active");
+    });
+    //닫는파트
+}
+
+// toggle(search)
+function toggleControl() {
+    var toggleTarget = null;
+    $("[type='button']").click(function () {
+        if ($(this).attr("data-toggle")) {
+            toggleTarget = "." + $(this).attr("data-toggle");
+            $(toggleTarget).removeClass("active").toggle();
+        }
+    });
+}
+
+// main bxSlider
+function mainSlider() {
+    $(".slider").bxSlider({
+        maxSlides: 3,//보이는 개체수
+        minSlides: 1,
+        moveSlides: 1,//개체가 하나씩 넘어가게
+        slideWidth: 310,//개체사이즈
+        slideMargin: 25, //개체사이 간격
+        pager: true, //? default: true. pager 켜기/끄기
+        pagerType: 'short', //? default : full-circle, short-count
+        infiniteLoop: false, //? default : true. slide간 전환 무한반복.(fade)
+    });
+}
+
+// mobile menu
+function mobileMenu() {
+    $("nav > ul > li").mouseenter(function () {
+        $(this).find(".sub").stop(true, true).slideDown();
+    });
+
+    $("nav > ul > li").mouseleave(function () {
+        $(this).find(".sub").stop(true, true).slideUp();
+    });
+}
+
+// mobile footer
+function mobileFooter(target) {
+    $(target).click(function () {
+        $(this).toggleClass("active");
+    });
+}
+
+
+
+
+// password
+function eyeIcon() {
+    // ? target.attr() = 대상의 모든 attr을 불러와 / target.attr("attr이름") = 대상의 해당 attr을 불러와/
+    // ? target.attr("attr이름","attr값") = 대상의 해당 attr 값을 바꿔라
+    var $toggleTarget = $(".material-symbols-outlined.toggleVisible");
+    var $toggleInput = $("#password");
+    var toggleStatus = false;
+
+    $toggleTarget.click(function () {
+        toggleStatus = !toggleStatus;
+
+        if (toggleStatus == true) {
+            $toggleTarget.val("visibility");
+            $toggleInput.attr("type", "text");
+        } else {
+            $toggleTarget.val("visibility_off");
+            $toggleInput.attr("type", "password");
+        }
+    });
+}
+
+// 2단 팝업
+function nestedPopup() {
+    var firstPopup = null;
+    var lastPopup = null;
+    $("[type='button']").click(function () {
+        if ($(this).attr("data-nested")) {
+            firstPopup = "." + $(this).attr("data-nested");
+            $(firstPopup).addClass("active");
+        }
+        // 1단 팝업 열기
+    });
+    $("[type='button']").click(function () {
+        if ($(this).attr("data-stacked")) {
+            lastPopup = "." + $(this).attr("data-stacked");
+            $(lastPopup).addClass("active");
+        }
+        // 2단 팝업 열기
+        else if ($(this).attr("data-sub")) {
+            lastPopup = "." + $(this).attr("data-sub");
+            $(lastPopup).toggleClass("active");
+        }
+        // 2단 팝업 토글
+    });
+    $(".bCloseBtn, .yesCloseBtn").click(function () {
+        $(firstPopup).removeClass("active");
+        $(lastPopup).removeClass("active");
+    });
+    // 2단 모두 닫힘
+    $(".noCloseBtn").click(function () {
+        $(lastPopup).removeClass("active");
+    });
+    //  2단 한단 닫힘
+    // Jquery - siblings, find, parents, child...
+}
+
+
+
+
+
+// list fiter
+function filterControl() {
+    var filterPopup = null;
+    $("[data-filter]").click(function () {
+        if ($(this).attr("data-filter")) {
+            filterPopup = "." + $(this).attr("data-filter");
+            $(filterPopup).toggleClass("active");
+            $(this).toggleClass("active");
+        }
+    });
+    // 열기
+    $(".bCloseBtn").click(function () {
+        $(filterPopup).removeClass("active");
+        $("[data-filter]").removeClass("active");
+    });
+    // 닫기
+    $(".uln").click(function () {
+        $(".filterPanel input[type='checkbox']").prop("checked", false);
+    });
+    // 체크표시 초기화
+}
+
+// mobile filter
+function mobileFilter() {
+    $(".filterPanel fieldset legend").click(function () {
+        $(this).siblings(".filterSub").toggleClass("active");
+    });
+}
+
+// mobile list accComponent
+function listaccControl(target) {
+    $(target).click(function () {
+        $(this).toggleClass("active");
+    });
+}
+
+// listPage hover
+function listHover() {
+    $("li figure img").each(function () {
+        var listimg = $(this);
+        var original = listimg.attr("src");
+        listimg.on("mouseenter", function () {
+            listimg.attr("src", original.replace(".png", "_hover.png"));
+        });
+        listimg.on("mouseleave", function () {
+            listimg.attr("src", original);
+        });
+    });
+}
+
+//listPage Promotion bxSlider
+function PromotionSlider() {
+    $(".recommendSlider").bxSlider({
+        maxSlides: 1,//보이는 개체수
+        minSlides: 1,
+        moveSlides: 1,//개체가 하나씩 넘어가게
+        slideWidth: 590,//개체사이즈
+        // slideMargin: 25, //개체사이 간격
+        pager: true, //? default: true. pager 켜기/끄기
+        pagerType: 'short', //? default : full-circle, short-count
+        infiniteLoop: false, //? default : true. slide간 전환 무한반복.(fade)
+    });
+}
+
+//listPage joy bxSlider
+function joySlider() {
+    $(".enjoySlider").bxSlider({
+        maxSlides: 4,//보이는 개체수
+        minSlides: 1,
+        moveSlides: 1,//개체가 하나씩 넘어가게
+        slideWidth: 336,//개체사이즈
+        slideMargin: 20, //개체사이 간격
+        pager: true, //? default: true. pager 켜기/끄기
+        pagerType: 'short', //? default : full-circle, short-count
+        infiniteLoop: false, //? default : true. slide간 전환 무한반복.(fade)
+    });
+}
+
+
+
+
+
+// back button
+function backControl() {
+    $(".backBtn").click(function () {
+        history.back();
+    });
+}
+
+// tab menu
+function tabMenu() {
+    var activeTab = null;
+    $(".tabMenu li, [data-tabNumb]").click(function () {
+        activeTab = $(this).attr("data-tabNumb");
+        $(".tabMenu li, [data-tabNumb]").removeClass("activated");
+        $(this).addClass("activated");
+        $(".tabPage").removeClass("activated");
+        $("#" + activeTab).addClass("activated");
+    });
+}
+
+// accComponent
+function accControl(target) {
+    $(target).click(function () {
+        $(this).toggleClass("active");
+    });
+}
+
+// 이미 최상단에 $(function(){}) = $(document).ready(function(){}) 이기 때문에 실행문은 위에 다 정리해서 놓으면 되고 외 function들만 밖에 위치 해야함
+
+// 그 이후에 남아있는 잔여 script기능 구현해오되 잘 안되는 구간은 따로 정리해서 가져오기
+
+// detailed Image
+function imgSwap() {
+    var imgSrc = null;
+
+    $(".detailedImage img").click(function () {
+
+        imgSrc = $(this).attr("src");
+        // 1. thumbNail click 시 -> 자신의 attr중 src값을 빼서 저장.
+        $(".detailContainer aside figure img").attr("src", imgSrc);
+        // 2. 크게 보이는 이미지 대상에 attr중 src값을 1번의 값으로 바꿔 끼기
+        // * animate-> 함수 사용하고 싶으면 사용해도 됨.
+
+    });
+}
+
+function countControl() {
+    // 1. 변수 - 현재 #qtyValue의 value를 가져와 담는다.
+    // 2. 클릭이벤트 2개 -> +, -
+    // 3. + 이벤트함수에서는 1번변수 당겨서 value에 ++
+    // 4. - 이벤트함수에서는 1번함수 당겨서 value에 --
+
+  $(".qtyComponent").each(function(){
+
+    var $countNum = $(this).find("input[type='number']"); 
+
+    $(this).find("input[type='button']").click(function(){ 
+
+      var count = parseInt($countNum.val()); 
+
+      if ($(this).val() == "+") { 
+         if (count < 99) count++;    
+      }
+
+      if ($(this).val() == "-") { 
+        if (count > 1) count--;    
+      }
+
+      $countNum.val(count);
+    });
+  });
+}
---
+++ js/old.js
@@ -0,0 +1,334 @@
+
+
+// 패널
+document.addEventListener('DOMContentLoaded', () => {
+    const openButtons = document.querySelectorAll('[data-panel]');
+    openButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const panelClass = button.getAttribute('data-panel');
+            const panel = document.querySelector(`.${panelClass}`);
+            if (panel) {
+                panel.classList.add('is-open');
+            }
+            const closeBtn = panel.querySelector('.bCloseBtn');
+            if (closeBtn) {
+                closeBtn.addEventListener('click', () => {
+                    panel.classList.remove('is-open');
+                });
+            }
+        });
+    });
+});
+
+// 팝업
+document.addEventListener('DOMContentLoaded', () => {
+    const openButtons = document.querySelectorAll('[data-popup]');
+    openButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const popupClass = button.getAttribute('data-popup');
+            const popup = document.querySelector(`.${popupClass}`);
+            if (popup) {
+                popup.classList.add('is-open');
+            }
+            const closeBtn = popup.querySelector('.bCloseBtn, .wCloseBtn, .closeBtn');
+            if (closeBtn) {
+                closeBtn.addEventListener('click', () => {
+                    popup.classList.remove('is-open');
+                });
+            }
+        });
+    });
+});
+
+
+// 2단이상 팝업은 규칙을 위 함수와 공유할 수 없음. 별도로 제작 해야함
+function nestedPopup() {
+    var currentPopup = null;
+    $("[type='button']").click(function () {
+        if ($(this).attr("data-nested")) {
+            currentPopup = "." + $(this).attr("data-nested");
+            $(currentPopup).addClass("active");
+        }
+    });
+    // 1팝업여트파트
+    $("[type='button']").click(function () {
+        if ($(this).attr("data-stacked")) {
+            currentPopup = "." + $(this).attr("data-stacked");
+            $(currentPopup).addClass("active");
+
+
+        } else if ($(this).attr("data-sub")) {
+            currentPopup = "." + $(this).attr("data-sub");
+            $(currentPopup).toggleClass("active");
+        }
+    });
+    // // 2팝업여닫는파트
+
+    $(".bCloseBtn").click(function () {
+        $(currentPopup).removeClass("active");
+    });
+    //1팝업닫는파트
+}
+// 디테일 이미지(책보고 함)
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     const mainImage = document.querySelector('.detailContainer aside figure img');
+//     const thumbImages = document.querySelectorAll('.detailedImage img');
+
+//     thumbImages.forEach((thumbImage) => {
+//         thumbImage.addEventListener('click', (event) => {
+//             mainImage.src = event.target.src;
+//             mainImage.animate({ opacity: [0, 1] }, 500);
+//         });
+//     });
+
+// });
+
+
+
+function productControl() {
+// 1) 처음 로드시: 현재 활성화된(activated) 컬러의 첫 label 텍스트 가져오기
+var $img = $(".detailContainer div:first-of-type aside figure img").attr("src");
+var $firstLoad = $(".colorPage.activated li:first-of-type label").text();
+
+// 2) 파일명 교체하여 이미지 표시
+var $tuneName = $img.replace(".png", "_" + $firstLoad + ".png");
+$(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+
+// 3) 컬러 클릭 이벤트
+$(".colorPage li input[type=radio]").click(function(){
+    
+    // 3-1) 새로 클릭한 컬러의 label 텍스트 가져오기
+    var selectedColor = $(this).next("label").text();
+
+    // 3-2) 현재 이미지 src 다시 가져오기
+    var currentImg = $(".detailContainer div:first-of-type aside figure img").attr("src");
+
+    // 3-3) 파일명 교체
+    var newImg = currentImg.replace(/_[^_]+\.png$/, "_" + selectedColor + ".png");
+
+    // 3-4) 이미지 교체
+    $(".detailContainer div:first-of-type aside figure img").attr("src", newImg);
+});
+
+
+// 4) Planter 변경 시 해당 colorPage만 표시
+$('input[name="planter"]').change(function(){
+    var target = $(this).data("color");
+
+    // 모두 비활성화
+    $(".colorPage").removeClass("activated");
+
+    // 해당 colorPage 활성화
+    $("#" + target).addClass("activated");
+
+    // 활성화된 페이지의 첫 color label 가져오기
+    var firstColor = $("#" + target).find("li:first-of-type label").text();
+
+    // 현재 이미지 경로 다시 쓰기
+    var imgSrc = $(".detailContainer div:first-of-type aside figure img").attr("src");
+    var newImgSrc = imgSrc.replace(/_[^_]+\.png$/, "_" + firstColor + ".png");
+
+    $(".detailContainer div:first-of-type aside figure img").attr("src", newImgSrc);
+});
+
+
+
+}
+
+
+
+function productControl() {
+    var largeImg = $(".detailContainer div:first-of-type aside figure img");
+
+    var $firstLoad = $(".color .colorPage.activated li:first-of-type label").text();
+
+    var $tuneName = largeImg.attr("src").replace(".png", $firstLoad + ".png");
+    largeImg.attr("src", $tuneName);
+
+    // label 클릭 이벤트 — 여기만 있으면 됨!
+    $(".color .colorPage li label").click(function () {
+
+        $firstLoad = $(this).text();
+        $tuneName = largeImg.attr("src").replace(/(.*)_.*\.png$/, "$1_" + $firstLoad + ".png");
+        largeImg.attr("src", $tuneName);
+
+        // active처리
+        $(".color .colorPage li label").removeClass("active");
+        $(this).addClass("active");
+    });
+}
+// detailed product
+// 참고해서 detail 작업해보기
+
+function productControl() {
+    var $img = $(".detailContainer div:first-of-type aside figure img").attr("src");
+
+    var $firstLoad = $(".color .colorPage.activated li:first-of-type label").text();
+
+    var $tuneName = $(".detailContainer div:first-of-type aside figure img").attr("src").replace(".png", $firstLoad + ".png");
+    // var $tuneName = ($(".detailContainer div:first-of-type aside figure img").attr("src") || "").replace(".png", $firstLoad + ".png"); // 안되었을때 이걸로 하니깐 작동됨 머지??
+
+
+    $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+    $(".color .colorPage li label").click(function () {
+
+        $firstLoad = $(this).text();
+
+        $tuneName = $img.replace(".png", $firstLoad + ".png");
+
+
+        $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+        $(this).closest(".colorPage").find("label").removeClass("active");
+        $(this).addClass("active");
+    });
+}
+
+
+
+
+
+function productControl() {
+    var $img = $(".detailContainer div:first-of-type aside figure img").attr("src");
+    var $planter = $('input[name="planter"]').attr("id");
+    // 1. planter 이름 당기기.
+    var $color = $(".color .colorPage.activated li:first-of-type label").attr("class");
+
+    var $tuneName = $(".detailContainer div:first-of-type aside figure img").attr("src").replace(".png", "_" + $planter + "_" + $color + ".png");
+    // 2. planter 이름까지 조립되게 변수+변수
+
+    $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+    $('input[name="planter"]').click(function () {
+        $planter = $(this).attr("id");
+
+        var colorTab = $(this).attr("data-color");
+
+        $(".colorPage").removeClass("activated");
+        $("#" + colorTab).addClass("activated");
+    });
+    // 4. planter 옵션세트 변경 할 수 있는 함수 필요.
+
+    $(".color .colorPage li label").click(function () {
+        $color = $(this).attr("class");
+
+        $tuneName = $img.replace(".png", "_" + $planter + "_" + $color + ".png");
+        // 3. planter 이름 변수가 가져와 져서 현재 $fistLoad와 조합되야함.
+
+        $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+        $(this).closest(".colorPage").find("label").removeClass("active");
+        $(this).addClass("active");
+    });
+}
+
+디테일 선택 완성본
+function productControl() {
+
+    var $img = $(".detailContainer div:first-of-type aside figure img").attr("src");
+    if ($('input[name="planter"]').length === 0) {
+        return; // 그냥 종료 → 기본 이미지 그대로 유지
+    }
+    var $planter = $('input[name="planter"]:checked').attr("id");
+
+    var colorTab = $('input[name="planter"]:checked').attr("data-color");
+    $(".colorPage").removeClass("activated");
+    $("#" + colorTab).addClass("activated");
+
+    var $color = $(".colorPage.activated label:not(.out)").first().text();
+
+    var $tuneName = $img.replace(".png", "_" + $planter + "_" + $color + ".png");
+    $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+    $('input[name="planter"]').click(function () {
+        $planter = $(this).attr("id");
+
+        var colorTab = $(this).attr("data-color");
+        $(".colorPage").removeClass("activated");
+        $("#" + colorTab).addClass("activated");
+
+        $color = $(".colorPage.activated label:not(.out)").first().text();
+
+        $tuneName = $img.replace(".png", "_" + $planter + "_" + $color + ".png");
+        $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+    });
+
+    $(".color .colorPage li label").click(function () {
+        $color = $(this).text();
+
+        $tuneName = $img.replace(".png", "_" + $planter + "_" + $color + ".png");
+        $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+        $(this).closest(".colorPage").find("label").removeClass("active");
+        $(this).addClass("active");
+    });
+}
+
+    // 컬러 선택
+    $(".color .colorPage li label").click(function () {
+        $color = $(this).attr("class").split(" ")[0];
+
+        $(".detailContainer div:first-of-type aside figure img")
+            .attr("src", baseImg + "_" + $planter + "_" + $color + ".png");
+
+        $(this).closest(".colorPage").find("label").removeClass("active");
+        $(this).addClass("active");
+    });
+}
+function countControl() {
+  $(".qtyComponent .plus, .qtyComponent .minus").click(function () {
+    var number = $(this).siblings("input[type='number']");
+    var current = parseInt(number.val());
+
+    if ($(this).hasClass("plus") && current < 99) {
+      number.val(current + 1);
+    }
+    if ($(this).hasClass("minus") && current > 1) {
+      number.val(current - 1);
+    }
+  });
+}
+
+
+
+
+// 과제
+// function productControl() {
+//     var $img = $(".detailContainer div:first-of-type aside figure img").attr("src");
+//     var $planter = $('input[name="planter"]:checked').attr("id");
+//     // 1. planter 이름 당기기.
+//     var $color = $(".colorPage.activated label:not(.out)").first().text();
+
+//     var $tuneName = $(".detailContainer div:first-of-type aside figure img").attr("src").replace(".png", "_" + $planter + "_" + $color + ".png");
+//     // 2. planter 이름까지 조립되게 변수+변수
+//     $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+//     $('input[name="planter"]').click(function () {
+//         $planter = $(this).attr("id");
+
+//         var colorTab = $(this).attr("data-color");
+
+//         $(".colorPage").removeClass("activated");
+//         $("#" + colorTab).addClass("activated");
+//         // $color = $(".colorPage.activated li:first-of-type label").text();
+//         $color = $(".colorPage.activated label:not(.out)").first().text();
+//         $tuneName = $img.replace(".png", "_" + $planter + "_" + $color + ".png");
+//         $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+//     });
+//     // 4. planter 옵션세트 변경 할 수 있는 함수 필요.
+
+//     $(".color .colorPage li label").click(function () {
+//         $color = $(this).attr("class");
+
+//         $tuneName = $img.replace(".png", "_" + $planter + "_" + $color + ".png");
+//         // 3. planter 이름 변수가 가져와 져서 현재 $fistLoad와 조합되야함.
+
+//         $(".detailContainer div:first-of-type aside figure img").attr("src", $tuneName);
+
+//         $(this).closest(".colorPage").find("label").removeClass("active");
+//         $(this).addClass("active");
+//     });
+// }
